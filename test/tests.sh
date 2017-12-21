5b7a2abde39d77655cd19bf390e93d34979ce807
David Goldstein <david.goldstein@deciphernow.com>
2017-12-21 13:42:54 -0600

    test bolt.db

--- test/tests.sh
+++ test/tests.sh
@@ -1,15 +1,18 @@
+# url=https://salty-sierra-82911.herokuapp.com/visits
+url=http://localhost:5000/visits
+
 echo "GET /VISTS"
-curl http://localhost:5000/visits
+curl "${url}"
 echo ""
 
 echo "POST /VISTS"
 curl -H \
  "Content-Type: application/json" \
  -X POST -d '{"ipAddress": "127.0.0.1", "location": "minneapolis"}' \
- http://localhost:5000/visits
+ "${url}"
 echo ""
 
 
 echo "GET /VISTS"
-curl http://localhost:5000/visits
+curl "${url}"
 echo ""
